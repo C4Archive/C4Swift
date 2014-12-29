c984749baa5cd53d81f9d810c9338467bf38ca33
-- C4 -- <info@c4ios.com>
2014-12-29 13:05:02 -0800

    Adding C4Ellipse, removing Ellipse.swift

--- C4Swift/ViewController.swift
+++ C4Swift/ViewController.swift
@@ -15,8 +15,8 @@ import C4Animation
 
 class ViewController: UIViewController {
     override func viewDidLoad() {
-        let rect = C4Rectangle(frame: C4Rect(100,100,100,100))
-        rect.corner = C4Size(15,15)
+        let rect = C4Ellipse(frame: C4Rect(100,100,100,100))
+//        rect.corner = C4Size(15,15)
         view.add(rect)
     }
 }
