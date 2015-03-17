ffd4af835770a7f06f28d12d52a6748904292797
-- C4 -- <info@c4ios.com>
2015-03-17 12:43:49 -0700

    4 layers of parallax w/ 36x width

--- C4Swift/ViewController.swift
+++ C4Swift/ViewController.swift
@@ -12,14 +12,23 @@ import C4Core
 import C4Animation
 
 class ViewController: UIViewController {
+    var scrv = InfiniteScrollView()
+    var ctxContentOffset = 0
+    var sv0 = InfiniteScrollView()
+    var sv1 = InfiniteScrollView()
+    var sv2 = InfiniteScrollView()
+    var sv3 = InfiniteScrollView()
 
     override func viewDidLoad() {
-
-        let sv1 = UIScrollView(frame: CGRect(canvas.frame))
-        let sv2 = UIScrollView(frame: CGRect(canvas.frame))
-        let sv3 = UIScrollView(frame: CGRect(canvas.frame))
-
-        let size = C4Size(canvas.width*12,canvas.height)
+        canvas.backgroundColor = C4Color(red:0.078, green:0.118, blue:0.306, alpha:1.0)
+        
+        sv0.frame = CGRect(canvas.frame)
+        sv1.frame = CGRect(canvas.frame)
+        sv2.frame = CGRect(canvas.frame)
+        sv3.frame = CGRect(canvas.frame)
+        
+        let size = C4Size(canvas.width*36,canvas.height)
+        sv0.contentSize = CGSize(size)
         sv1.contentSize = CGSize(size)
         sv2.contentSize = CGSize(size)
         sv3.contentSize = CGSize(size)
@@ -28,7 +37,7 @@ class ViewController: UIViewController {
         var pos2 = C4Point(0,215)
         var pos3 = C4Point(171, 460)
         var pos4 = C4Point()
-        for i in 0...11 {
+        for i in 0...35 {
             let img1 = C4Image("star3")
             let img2 = C4Image("star3")
             let img3 = C4Image("star3")
@@ -52,7 +61,7 @@ class ViewController: UIViewController {
         pos4.x = 90
         pos4.y = 541
         
-        for i in 0...11 {
+        for i in 0...35 {
             let img1 = C4Image("star2")
             let img2 = C4Image("star2")
             let img3 = C4Image("star2")
@@ -70,14 +79,41 @@ class ViewController: UIViewController {
         }
         canvas.add(sv2)
 
-        pos1.x = 23
-        pos1.y = 50
-        pos2.x = 215
-        pos2.y = 11
-        pos3.x = 184
-        pos3.y = 556
+        pos1.x = 162
+        pos1.y = 51
+        pos2.x = 148
+        pos2.y = 308
+        pos3.x = 97
+        pos3.y = 170
+        pos4.x = 60
+        pos4.y = 341
         
-        for i in 0...11 {
+        for i in 0...35 {
+            let img1 = C4Image("star2")
+            let img2 = C4Image("star2")
+            let img3 = C4Image("star2")
+            let img4 = C4Image("star2")
+            
+            img1.origin = C4Point(pos1.x + Double(i) * canvas.width, pos1.y)
+            img2.origin = C4Point(pos2.x + Double(i) * canvas.width, pos2.y)
+            img3.origin = C4Point(pos3.x + Double(i) * canvas.width, pos3.y)
+            img4.origin = C4Point(pos4.x + Double(i) * canvas.width, pos4.y)
+            
+            sv1.add(img1)
+            sv1.add(img2)
+            sv1.add(img3)
+            sv1.add(img4)
+        }
+        canvas.add(sv1)
+        
+        pos1.x = 93
+        pos1.y = 150
+        pos2.x = 55
+        pos2.y = 11
+        pos3.x = 204
+        pos3.y = 446
+        
+        for i in 0...35 {
             let img1 = C4Image("star1")
             let img2 = C4Image("star1")
             let img3 = C4Image("star1")
@@ -86,11 +122,30 @@ class ViewController: UIViewController {
             img2.origin = C4Point(pos2.x + Double(i) * canvas.width, pos2.y)
             img3.origin = C4Point(pos3.x + Double(i) * canvas.width, pos3.y)
             
-            sv1.add(img1)
-            sv1.add(img2)
-            sv1.add(img3)
+            sv0.add(img1)
+            sv0.add(img2)
+            sv0.add(img3)
         }
-        canvas.add(sv1)
+        canvas.add(sv0)
 
+        let vign = C4Image("vignette")
+        vign.center = canvas.center
+//        canvas.add(vign)
+        vign.interactionEnabled = false
+        sv0.addObserver(self, forKeyPath: "contentOffset", options: .New, context: &ctxContentOffset)
+    }
+    
+    
+    
+    override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+        if context == &ctxContentOffset {
+            var mod = sv0.contentOffset
+            mod.x *= 0.8
+            sv1.contentOffset = mod
+            mod.x *= 0.8
+            sv2.contentOffset = mod
+            mod.x *= 0.8
+            sv3.contentOffset = mod
+        }
     }
 }
