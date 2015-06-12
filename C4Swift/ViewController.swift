0617434114260dece9dd8999aa92f28efd7ea337
-- C4 -- <travis@c4ios.com>
2015-06-12 16:02:27 -0700

    Adding first screen, with rotatable needle.

--- C4Swift/ViewController.swift
+++ C4Swift/ViewController.swift
@@ -14,6 +14,9 @@ import C4Animation
 class ViewController: C4CanvasController {
     let bg = C4Image("statusBarBG1px")
     let alertBar = AlertBar()
+    let needle = C4Image("needle")
+    let screen1 = C4Image("screen1")
+    let rotationScale = -1.495*M_PI
     
     override func setup() {
         view.backgroundColor = UIColor(patternImage: bg.uiimage)
@@ -30,6 +33,12 @@ class ViewController: C4CanvasController {
 
         self.navigationController?.view.add(alertBar.canvas)
         
+        screen1.origin = C4Point(0,64)
+        canvas.add(screen1)
+
+        needle.anchorPoint = C4Point(0.88,0.12)
+        needle.center = C4Point(477,236)
+        canvas.add(needle)
     }
     
     func reset() {
