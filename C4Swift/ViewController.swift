a534ef4c8938c3aa6dbf6fb4ef7a5390f7e13303
-- C4 -- <travis@c4ios.com>
2015-06-12 14:18:46 -0700

    Cleaning up viewcontroller

---
+++ C4Swift/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  C4Swift
+//
+//  Created by travis on 2014-10-28.
+//  Copyright (c) 2014 C4. All rights reserved.
+//
+
+import UIKit
+import C4UI
+import C4Core
+import C4Animation
+
+class ViewController: C4CanvasController {
+    override func setup() {
+        
+    }
+}
