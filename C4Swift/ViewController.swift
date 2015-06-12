61e93d32ea67aebb720be4db5c15e394e6d7e907
-- C4 -- <travis@c4ios.com>
2015-06-12 16:31:43 -0700

    Add brake temp data flip functionality

--- C4Swift/ViewController.swift
+++ C4Swift/ViewController.swift
@@ -17,6 +17,10 @@ class ViewController: C4CanvasController {
     let needle = C4Image("needle")
     let screen1 = C4Image("screen1")
     let rotationScale = -1.495*M_PI
+    let brakeImageContainer = C4View()
+    let brakeTemp01 = C4Image("brakeTemp01")
+    let brakeTemp02 = C4Image("brakeTemp02")
+    let brakeTempOverlay = C4Image("brakeTempOverlay")
     
     override func setup() {
         view.backgroundColor = UIColor(patternImage: bg.uiimage)
@@ -39,9 +43,44 @@ class ViewController: C4CanvasController {
         needle.anchorPoint = C4Point(0.88,0.12)
         needle.center = C4Point(477,236)
         canvas.add(needle)
+        
+        brakeTemp01.interactionEnabled = true
+        
+        brakeTemp01.addTapGestureRecognizer { (location, state) -> () in
+            self.flip01()
+        }
+        
+        brakeTemp02.addTapGestureRecognizer { (location, state) -> () in
+            self.flip02()
+        }
+        
+        brakeTempOverlay.addTapGestureRecognizer { (location, state) -> () in
+            self.revealPopup()
+        }
+        
+        brakeTemp02.interactionEnabled = true
+        brakeTempOverlay.interactionEnabled = true
+        brakeTemp02.add(brakeTempOverlay)
+        
+        brakeImageContainer.frame = brakeTemp01.bounds
+        brakeImageContainer.origin = C4Point(461,572)
+        brakeImageContainer.add(brakeTemp01)
+        canvas.add(brakeImageContainer)
     }
     
     func reset() {
+        
+    }
+    
+    func flip01() {
+        UIView.transitionFromView(brakeTemp01.view, toView: brakeTemp02.view, duration: 0.25, options: UIViewAnimationOptions.TransitionFlipFromRight, completion: nil)
+    }
+    
+    func flip02() {
+        UIView.transitionFromView(brakeTemp02.view, toView: brakeTemp01.view, duration: 0.25, options: UIViewAnimationOptions.TransitionFlipFromLeft, completion: nil)
+    }
+    
+    func revealPopup() {
         
     }
 }
