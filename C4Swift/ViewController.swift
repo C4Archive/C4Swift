66d6be24827f1ff289e47aba2b897337ac89419d
travis <travis@c4ios.com>
2015-10-01 16:12:13 -0700

    Adds random shapes per Res2015 branding

--- C4Swift/ViewController.swift
+++ C4Swift/ViewController.swift
@@ -11,6 +11,7 @@ import C4
 
 class ViewController: C4CanvasController {
     var colors = [[C4Color]]()
+    
     override func setup() {
         canvas.backgroundColor = white
 
@@ -20,14 +21,64 @@ class ViewController: C4CanvasController {
         colors.append([C4Color(red: 0.199, green: 0.727, blue: 0.945, alpha: 1.0),C4Color(red: 0.303, green: 0.308, blue: 0.619, alpha: 1.0)])
         colors.append([C4Color(red: 0.904, green: 0.093, blue: 0.169, alpha: 1.0),C4Color(red: 0.915, green: 0.318, blue: 0.594, alpha: 1.0)])
         
+        let circle = createCircle
+        let tetris = createTetris
+        let line = createLine
+        let plus = createPlus
+
+        let shapes = [circle, tetris, line, plus]
+        canvas.addTapGestureRecognizer { (location, state) -> () in
+            self.addShape(shapes, location: location)
+        }
+        
+        self.addShapeAtRandomPoint(shapes)
     }
+    
+    func addShape(shapes: [()->C4Gradient], location: C4Point) {
+        C4ShapeLayer.disableActions = true
+        let index = random(below: shapes.count)
+        let method = shapes[index]
+        let newShape = method()
+        newShape.center = location
+        newShape.interactionEnabled = false
+        self.canvas.add(newShape)
+        C4ShapeLayer.disableActions = false
+        
+        delay(3.0) { () -> () in
+            let anim = C4ViewAnimation(duration: 0.25, animations: { () -> Void in
+                newShape.opacity = 0.0
+            })
+            anim.addCompletionObserver({ () -> Void in
+                newShape.removeFromSuperview()
+            })
+            anim.animate()
+        }
+    }
+    
+    func addShapeAtRandomPoint(shapes: [()->C4Gradient]) {
+        let x = random01() * canvas.width * 0.66 + canvas.width * 0.33/2.0
+        let y = random01() * canvas.height * 0.66 + canvas.height * 0.33/2.0
+        
+        addShape(shapes, location: C4Point(x,y))
+        delay(random01()*5.0+1.0) { () -> () in
+            self.addShapeAtRandomPoint(shapes)
+        }
+   }
     
     func createCircle() -> C4Gradient {
         let gr = C4Gradient(frame: C4Rect(0,0,canvas.height/3.0+40.0,canvas.height/3.0+40.0), colors: colors[random(below: colors.count)], locations: [0,1])
         let c = C4Circle(center: gr.center, radius: gr.width/2.0-20.0)
         c.lineWidth = 40.0
         c.fillColor = clear
+        c.lineCap = .Butt
+        c.strokeEnd = 0.0
         gr.layer!.mask = c.layer
+        
+        delay(0.25) { () -> () in
+            C4ViewAnimation(duration: 0.25, animations: { () -> Void in
+                c.strokeEnd = 1.0
+            }).animate()
+        }
         return(gr)
     }
     
@@ -48,29 +99,42 @@ class ViewController: C4CanvasController {
         t.lineWidth = 40.0
         t.fillColor = clear
         t.lineJoin = .Miter
+        t.strokeEnd = 0.0
         gr.layer!.mask = t.layer
         C4ShapeLayer.disableActions = false
+        
+        delay(0.25) { () -> () in
+            C4ViewAnimation(duration: 0.25, animations: { () -> Void in
+                t.strokeEnd = 1.0
+            }).animate()
+        }
         return(gr)
     }
     
     func createPlus() -> C4Gradient {
         let gr = C4Gradient(frame: C4Rect(0,0,canvas.height/6.0,canvas.height/6.0), colors: colors[random(below: colors.count)], locations: [0,1])
-        let line1 = C4Line([C4Point(0,gr.height/2.0),C4Point(gr.width,gr.height/2.0)])
-        line1.lineWidth = 40.0
-        line1.lineCap = .Butt
-        let line2 = C4Line([C4Point(gr.width/2.0,0),C4Point(gr.width/2.0,gr.height)])
-        line2.lineWidth = 40.0
-        line2.lineCap = .Butt
-        if let l2l = line2.layer {
-            line1.layer?.addSublayer(l2l)
-        }
-        gr.layer?.mask = line1.layer
+        let path = C4Path()
+        path.moveToPoint(C4Point(0,gr.height/2.0))
+        path.addLineToPoint(C4Point(gr.width,gr.height/2.0))
+        path.moveToPoint(C4Point(gr.width/2.0,0))
+        path.addLineToPoint(C4Point(gr.width/2.0,gr.height))
+        let plus = C4Shape(path)
+        
+        plus.lineWidth = 40.0
+        plus.lineCap = .Butt
+        plus.strokeEnd = 0.0
+        gr.layer?.mask = plus.layer
         
         if Bool(random(below: 2)) {
             let rotation = C4Transform.makeRotation(M_PI_4)
             gr.transform = rotation
         }
         
+        delay(0.25) { () -> () in
+            C4ViewAnimation(duration: 0.25, animations: { () -> Void in
+                plus.strokeEnd = 1.0
+            }).animate()
+        }
         return gr
     }
     
@@ -79,7 +143,15 @@ class ViewController: C4CanvasController {
         let line = C4Line([C4Point(0,gr.height/2.0),C4Point(gr.width,gr.height/2.0)])
         line.lineWidth = 40.0
         line.lineCap = .Butt
+        line.strokeEnd = 0.0
         gr.layer?.mask = line.layer
+
+        delay(0.25) { () -> () in
+            C4ViewAnimation(duration: 0.25, animations: { () -> Void in
+                line.strokeEnd = 1.0
+            }).animate()
+        }
+
         return gr
     }
 
