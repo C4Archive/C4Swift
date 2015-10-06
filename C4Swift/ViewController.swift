b6df783d084c6b3eabb62f3caefa1d08c00ce712
-- C4 -- <info@c4ios.com>
2015-10-05 19:51:51 -0700

    Updates project settings, adds pattern images

--- C4Swift/ViewController.swift
+++ C4Swift/ViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 import C4
 
 class ViewController: C4CanvasController {
-
     override func setup() {
-        //work your magic here
+        let b = C4Vector(x: self.canvas.center.x + 1, y: self.canvas.center.y)
+        let c = C4Vector(canvas.center)
+
+        canvas.addPanGestureRecognizer { (location, translation, velocity, state) -> () in
+            let a = C4Vector(location)
+            let angle = a.angleTo(b, basedOn: c)
+            let reflex = a.y < b.y ? 2*M_PI-angle : angle
+            print(reflex)
+        }
+    }
+}
+
+public extension C4Vector {
+    init(_ point: C4Point) {
+        self.init(x: point.x, y: point.y)
     }
 }
