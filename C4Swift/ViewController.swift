747899c9870c5553695af0292528544f3253522e
-- C4 -- <info@c4ios.com>
2016-01-13 00:19:16 -0800

    adds arrow class, and swipe functionality for adding arrows

--- C4Swift/ViewController.swift
+++ C4Swift/ViewController.swift
@@ -11,6 +11,73 @@ import C4
 
 class ViewController: C4CanvasController {
     override func setup() {
+        for i in 1...5 {
+            let left = canvas.addSwipeGestureRecognizer() { location, state, direction in
+                let a = Arrows(count: i, direction: direction)
+                a.center = location
+                self.canvas.add(a)
+            }
+            left.numberOfTouchesRequired = i
+            left.direction = .Left
 
+            let down = canvas.addSwipeGestureRecognizer() { location, state, direction in
+                let a = Arrows(count: i, direction: direction)
+                a.center = location
+                self.canvas.add(a)
+            }
+            down.numberOfTouchesRequired = i
+            down.direction = .Down
+
+            let right = canvas.addSwipeGestureRecognizer() { location, state, direction in
+                let a = Arrows(count: i, direction: direction)
+                a.center = location
+                self.canvas.add(a)
+            }
+            right.numberOfTouchesRequired = i
+
+            let up = canvas.addSwipeGestureRecognizer() { location, state, direction in
+                let a = Arrows(count: i, direction: direction)
+                a.center = location
+                self.canvas.add(a)
+            }
+            up.numberOfTouchesRequired = i
+            up.direction = .Up
+        }
+    }
+}
+
+class Arrows: C4View {
+    convenience init(count: Int, direction: UISwipeGestureRecognizerDirection) {
+        C4ShapeLayer.disableActions = true
+        self.init(frame: C4Rect(0,0,10,10))
+        switch direction {
+        case UISwipeGestureRecognizerDirection.Up:
+            self.transform = C4Transform.makeRotation(M_PI_2)
+        case UISwipeGestureRecognizerDirection.Left:
+            self.transform = C4Transform.makeRotation(M_PI)
+        case UISwipeGestureRecognizerDirection.Down:
+            self.transform = C4Transform.makeRotation(-M_PI_2)
+        default:
+            _ = ""
+        }
+
+        let offset = C4Point(0,-Double(count) / 2.0 * 10.0)
+        C4ShapeLayer.disableActions = false
+
+        var anims = [C4ViewAnimation]()
+        for i in 0..<count {
+            let point = offset + C4Point(0,10) * i
+            let c = C4Circle(center: point, radius: 5)
+            add(c)
+
+            let anim = C4ViewAnimation(duration: 0.25) {
+                c.center = C4Point(100,c.center.y)
+            }
+            anim.addCompletionObserver() {
+                c.removeFromSuperview()
+            }
+            anims.append(anim)
+        }
+        C4ViewAnimationGroup(animations: anims).animate()
     }
 }
