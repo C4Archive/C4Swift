da00a9be68aa84e9f12c34124cf7e903d9bbea2c
-- C4 -- <info@c4ios.com>
2016-01-12 22:20:06 -0800

    Adds images

--- C4Swift/ViewController.swift
+++ C4Swift/ViewController.swift
@@ -11,20 +11,6 @@ import C4
 
 class ViewController: C4CanvasController {
     override func setup() {
-        let b = C4Vector(x: self.canvas.center.x + 1, y: self.canvas.center.y)
-        let c = C4Vector(canvas.center)
 
-        canvas.addPanGestureRecognizer { (location, translation, velocity, state) -> () in
-            let a = C4Vector(location)
-            let angle = a.angleTo(b, basedOn: c)
-            let reflex = a.y < b.y ? 2*M_PI-angle : angle
-            print(reflex)
-        }
-    }
-}
-
-public extension C4Vector {
-    init(_ point: C4Point) {
-        self.init(x: point.x, y: point.y)
     }
 }
