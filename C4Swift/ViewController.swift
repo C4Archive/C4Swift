846df36080fbc0fc70ee79a86b255e70e8b1e79f
-- C4 -- <info@c4ios.com>
2016-01-14 23:58:05 -0800

    Adds crosshairs

--- C4Swift/ViewController.swift
+++ C4Swift/ViewController.swift
@@ -10,7 +10,35 @@ import UIKit
 import C4
 
 class ViewController: C4CanvasController {
-    override func setup() {
+    var p: UIPanGestureRecognizer!
+    var lines = [(C4Line,C4Line)]()
 
+    override func setup() {
+        for _ in 0..<5 {
+            let v = C4Line((C4Point(),C4Point(0,canvas.height)))
+            let h = C4Line((C4Point(),C4Point(canvas.width,0)))
+            lines.append((v,h))
+            canvas.add(v)
+            canvas.add(h)
+        }
+
+        canvas.backgroundColor = darkGray
+
+        canvas.addPanGestureRecognizer { (location, translation, velocity, state, locations) -> () in
+            for i in 0..<self.lines.count {
+                let v = self.lines[i].0
+                let h = self.lines[i].1
+
+                C4ViewAnimation(duration: 0) {
+                    if i < locations.count {
+                        v.origin = C4Point(locations[i].x,0)
+                        h.origin = C4Point(0,locations[i].y)
+                    } else {
+                        v.origin = C4Point(-1,0)
+                        h.origin = C4Point(0,-1)
+                    }
+                }.animate()
+            }
+        }
     }
 }
