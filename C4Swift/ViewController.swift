158354002ecce2f1541381505c12aa0606eb67a7
-- C4 -- <info@c4ios.com>
2016-01-21 11:42:43 -0800

    Test for gradient fill using graphics contexts

--- C4Swift/ViewController.swift
+++ C4Swift/ViewController.swift
@@ -11,6 +11,28 @@ import C4
 
 class ViewController: C4CanvasController {
     override func setup() {
+        let star = C4Star(center: canvas.center, pointCount: 10, innerRadius: 50, outerRadius: 80)
+        let g = C4Gradient(frame:star.frame, colors: [C4Blue, C4Pink])
+        star.gradientFill = g
+        let c = C4Circle(center: canvas.center, radius: 25)
 
+        c.gradientFill = g
+        canvas.add(star)
+        canvas.add(c)
+
+        var b = false
+        let a = C4ViewAnimation(duration:1.0) {
+            if b {
+                g.frame = star.frame
+                star.gradientFill = g
+            } else {
+                star.fillColor = C4Pink
+            }
+            b = !b
+        }
+
+        canvas.addTapGestureRecognizer { (location, state) -> () in
+            a.animate()
+        }
     }
 }
